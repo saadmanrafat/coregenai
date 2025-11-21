32a0b53b12f171ca3fe0b3c18813ce7d93ee0aa0
Saadman Rafat <saadman@outlook.com>
2025-11-22 02:26:09 +0600

    creating .env file if there isnt one

--- coregenai/__init__.py
+++ coregenai/__init__.py
@@ -1,12 +1,27 @@
 import platform
+import shutil
+from pathlib import Path
 
-if platform.python_version_tuple() <= ('3', '12'):
+if platform.python_version_tuple() < ("3", "12"):
     raise RuntimeError("coregenai requires Python 3.12 or higher")
+
+if not (Path(__file__).parent.parent / ".env").exists():
+    shutil.copy(
+        Path(__file__).parent.parent / ".env.example",
+        Path(__file__).parent.parent / ".env",
+    )
+    print(
+        "A default .env file has been automatically created. \n"
+        "Please review and update it with your configuration."
+    )
+
 
 from coregenai.config import GenConfig
 from coregenai.retry import with_retry
 from coregenai.interfaces import GenInterceptor
 from coregenai.core import CoreGenAI, SafetyViolationError
+
+__version__ = "0.3.1"
 
 
 __all__ = [
@@ -14,5 +29,5 @@ __all__ = [
     "with_retry",
     "GenInterceptor",
     "CoreGenAI",
-    "SafetyViolationError"
+    "SafetyViolationError",
 ]
